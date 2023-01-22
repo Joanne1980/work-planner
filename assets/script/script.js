b94a6b1f508d0ba79138fc93c9270a56fc191743
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-01-22 20:45:52 +0000

    updates made to JS code creating variables to help with coding

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,30 @@
+//current day 
 var today = moment()
 $('#currentDay'). text (today.format('DD MMMM YYYY'));
-//console.log(currentDay)
+console.log(currentDay)
 
-//var Time = "9, 10, 11, 12, 13, 14, 15, 16, 17"
+//create variables to target each timeblock
+var pastColor =""
+var presentColor =""
+var futureColor =""  
+var time = thisHour
+var thisHour = moment(time).format('H')
+var timeblocks = $('.timeblock')
+timeblocks.each(function(){
+
+var hour = $(this).attr('data-hour'); 
+hour = parseInt(hour, 10)
+thisHour = parseInt(thisHour, 10)
+
+if (hour < thisHour){
+    $(this).css('background-color', pastColor)
+} else if (hour == thisHour){
+    $(this).css('background-color', presentColor)    
+    } else {
+    $(this).css('background-color', futureColor)   
+ }
+})   
+//console.log(timeblock)
+
+
 
